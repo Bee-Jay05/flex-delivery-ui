3b0e3f7c2bc3b5d1acee12a867c3d5938870f3d6
Bee Jay <abiolaabeeola159@gmail.com>
2025-11-12 21:34:59 +0100

    minor

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ export default function Header(){
   return (
     <header className="header">
       <div className="header-left">
-        <button className="back-btn">✕</button>
+        <button className="back-btn"></button>
         <h1 className="title">Temp</h1>
       </div>
       <div className="header-right">
-        <div className="icons">⚙️ 👤</div>
+        <div className="icons"></div>
       </div>
     </header>
   );
